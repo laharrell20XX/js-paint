fb8f2a52a576768412fbf3598298c841a0db99f9
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-11-27 21:17:52 -0600

    improved drawing functionality so lines are drawn between the points on the canvas; added a way to clear the screen
    Additions/Changes:
    	- initialization:
    	added a variable drawnPoints that is initially an empty list (drawnPoints represents a list of recent points visible on the canvas); added an event listener to the window that clears the screen
    
    	- drawPoint() canvas event listener:
    	added some setup to drawnPoints as well as a call to drawLineSeg; the position of the mouse is only referenced from ev.offset;
    		- handler:
    		added else case to first ev.buttons test that constantly clears drawnPoints (to avoid lines being drawn from points left over, so every drawing from mousedown to mouse up is visibly seperated unless purposely overlapped); added a nested if case inside of the outer if case which tests if the size of drawnPoints.length is 2; when true, drawLineSeg is called with drawnPoints and the earliest point is removed from drawnPoints; when false, drawPoint is called, and an object (representing the drawn point) is added to drawnPoints; the object consists of two fields, x and y, where x is the x position of the cursor, and the y is the y position of the cursor;
    
    	- clear() event listener:
    	listens for a keypress and has a handler that involves an anonymous function that checks the value of the key pressed; if its "r" clear is called
    
    	- drawPoint():
    	changed name from draw to drawPoint to reflect accuracy of its functionality
    
    	- drawLineSeg():
    	function that draws a line segment between the points specified in drawnPoints
    
    		:args:
    		drawnPoints - a list of points visible on the canvas where a point is an object
    
    		:returns:
    		None - only canvas drawing is done here
    
    	canvasCtx is initialized similarly to drawPoint where lineCap and lineWidth are the same; creates variables lastPoint (the first item in drawnPoints) and curPoint (the last item in drawnPoints); draws a path where its starting point is at the lastPoint and ending point is at curPoint;
    
    	- clear():
    	function that draws a clearRect over the entire canvas

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 mouseDisplayer = document.querySelector("p.mouse-position");
 var canvas = document.querySelector("canvas");
+var drawnPoints = [];
 canvas.addEventListener("mousemove", ev => {
     if (ev.buttons == 1) {
-        draw(ev);
-        mouseDisplayer.innerText = `(${ev.clientX},${ev.clientY})`;
+        if (drawnPoints.length === 2) {
+            // console.log(drawnPoints);
+            drawLineSeg(drawnPoints);
+            drawnPoints.shift();
+        }
+        drawPoint(ev);
+        drawnPoints.push({ x: ev.offsetX, y: ev.offsetY });
+        mouseDisplayer.innerText = `(${ev.offsetX},${ev.offsetY})`;
+    } else {
+        drawnPoints = [];
     }
 });
-function draw(ev) {
+window.addEventListener("keypress", ev => {
+    if (ev.key.toLowerCase() == "r") {
+        clear();
+    }
+});
+function drawPoint(ev) {
     canvasCtx = canvas.getContext("2d");
-    canvasCtx.lineWidth = 5.0;
+    canvasCtx.lineWidth = 0.0;
     canvasCtx.lineCap = "round";
     canvasCtx.beginPath();
     canvasCtx.moveTo(ev.offsetX, ev.offsetY);
     canvasCtx.lineTo(ev.offsetX, ev.offsetY);
     canvasCtx.stroke();
+}
+function drawLineSeg(drawnPoints) {
+    canvasCtx = canvas.getContext("2d");
+    canvasCtx.lineWidth = 5.0;
+    canvasCtx.lineCap = "round";
+    lastPoint = drawnPoints[0];
+    curPoint = drawnPoints[1];
+    canvasCtx.beginPath();
+    canvasCtx.moveTo(lastPoint.x, lastPoint.y);
+    canvasCtx.lineTo(curPoint.x, curPoint.y);
+    canvasCtx.stroke();
+}
+function clear(ev) {
+    canvasCtx = canvas.getContext("2d");
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 }
